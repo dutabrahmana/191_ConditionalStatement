1891331412b284cd73060aa394ded5c9abe5414c
dutabrahmana <dutalinggau25@gmail.com>
2024-03-05 12:34:53 +0700

    Add project files.

---
+++ PraktikumPertemuan22/Praktikum Part1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int nBilangan;
+	string status;
+
+	srand(time(0));
+
+	nBilangan = rand() % 6;
+
+	if (nBilangan % 2 == 0) {
+		status = "Genap";
+
+	}
+	else {
+		status = "Ganjil";
+	}
+	cout << "Bilangan = " << nBilangan << endl;
+
+	cout << "status bilangannya adalah = " << status << endl;
+	
+	return 0;
+
+}
